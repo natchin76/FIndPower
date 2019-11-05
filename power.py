c4af8b362bba430d9ffe8bbaa10c03aa56c2d247
natchin76 <57409149+natchin76@users.noreply.github.com>
2019-11-06 00:12:15 +0530

    Update power.py

--- power.py
+++ power.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Nov  5 22:58:20 2019
-Find the index at which a given power of 2 appears in a list
+Find the index at which a given power of 2 appears in a list(found flag removed)
 @author: CHINTAN
 """
 L = [1, 2, 4, 8, 16, 32, 64]
-X = 5
+X = 3
 found = False   
 i = 0
-while not found and i < len(L):
+while i < len(L):
     if 2 ** X == L[i]:
-        found = True
+        print('at index', i)
+        break
     else:
         i = i+1
-    if found:
-        print('at index', i)
-    else:
         print(X, 'not found')
